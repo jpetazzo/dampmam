9547a9712c1b0b330a19764098e0445b32237caa
Jérôme Petazzoni <jerome.petazzoni@docker.com>
2017-03-14 22:26:35 -0700

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<html><body>
+
+<h1>Hello, I am the title</h1>
+
+This page is hosted by your dampmam container! How awesome is that!
+
+<?
+echo "This is some PHP code! 2+2=".(2+2);
+?>
+
+</html></body>
