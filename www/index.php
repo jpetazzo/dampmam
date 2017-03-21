eabd4a6b7d65d4e6de9b625c2aa5cdb84f8c3c94
juliegund <jgunderson@taos.com>
2017-03-21 20:43:21 +0000

    fixed the math

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html><body>
 
-<h1><p style="color:#800080">Sparkles are awesome"<p/><h1>
+<h1><p style="color:#800080">Sparkles are awesome<p/><h1>
 
 This page is hosted by your dampmam container! How awesome is that!
 
